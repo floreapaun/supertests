e98b534b9e2ef04ae9e31106467d7dc3bbf10486
kajal <kajalbhati085@gmail.com>
2019-01-20 12:31:03 +0530

    slider

--- src/app/layout/welcome/welcome.component.ts
+++ src/app/layout/welcome/welcome.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from '../../services/auth.service';
+import { ListPicker } from "tns-core-modules/ui/list-picker";
+
 
 @Component({
   selector: 'app-welcome',
@@ -12,12 +14,12 @@ export class WelcomeComponent implements OnInit {
     name: 'test',
     email: 'kajal@gmail',
     test :''
-  }
+  };
   constructor(private router: Router,
-              private auth:AuthService) { }
+              private auth: AuthService) { }
 
   ngOnInit() {
-    this.auth.name().subscribe(res=>{
+    this.auth.name().subscribe(res => {
       console.log('Auth',res);
     })
    
@@ -28,6 +30,12 @@ export class WelcomeComponent implements OnInit {
     console.log('noti');
     window.localStorage.setItem('user', 'kajal');
     this.router.navigate(['/notification']);
+  }
+
+
+  selectedIndexChanged(args){
+    const picker = <ListPicker>args.object;
+    this.picker = this.typeTestList[picker.selectedIndex];
   }
 
 }
