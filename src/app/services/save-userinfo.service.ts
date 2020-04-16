181215eb83791f5950900d4013ac4b815858a3b2
serj99 <go_with_the_flow99@yahoo.com>
2020-04-16 17:07:51 +0300

    API access method updated.

--- src/app/services/save-userinfo.service.ts
+++ src/app/services/save-userinfo.service.ts
@@ -4,12 +4,11 @@ import { HttpClient} from '@angular/common/http'
 
 
 export class SaveUserinfoService {
-
+    url = 'http://localhost:3000';
 
     constructor( private http: HttpClient  ){ 
 
     }
-
 
     saveDataToServer(user){
         this.http.post('https://ipriksha-be1b8.firebaseio.com/user.json',user).subscribe(res=>{
@@ -17,12 +16,10 @@ export class SaveUserinfoService {
         })
       }
 
-      saveScoreDataToServer(user){
-        this.http.post('https://ipriksha-be1b8.firebaseio.com/user.json',user).subscribe(res=>{
-          // console.log(res);
-        })
-      }
-
-
+    saveScoreDataToServer(data){
+      this.http.post(`${this.url}/user/saveFinishedTest`, data).subscribe(res=>{
+         console.log(res);
+      })
+    }
 
 }
