1ecb138135739223d75d5cc891f0d2a3c8e3b603
serj99 <go_with_the_flow99@yahoo.com>
2020-04-29 13:18:42 +0300

    Finished tests table added.

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -10,6 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from 'src/app/material-module';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import User from '../../models/User';
+import { LoaderService } from 'src/app/services/loader.service';
+import { FinishedTestObj } from 'src/app/finishedtest-object';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,14 +20,30 @@ import User from '../../models/User';
 })
 export class DashboardComponent implements OnInit {
   user: User; 
+  finishedTestsList: Array < FinishedTestObj >;
 
   constructor(private router: Router,
     private http: HttpClient,
     private authenticationService: AuthenticationService,
+    private loaderSer: LoaderService,
    ) {}
 
   ngOnInit(): void {
     this.user = this.authenticationService.currentUserValue; 
+    this.getFinishedTestsList(); 
   }
+
+  getFinishedTestsList(): void {
+    this.loaderSer.show(true);
+    this.http.get < Array < FinishedTestObj >> (`http://localhost:3000/user/finishedTestsList`, {
+        responseType: 'json'
+      })
+      .subscribe(
+        (res: Array < FinishedTestObj > ) => {
+          this.loaderSer.show(false);
+          this.finishedTestsList = res;
+        });
+  }
+
 
 }
