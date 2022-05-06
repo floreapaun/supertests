05eefeafec94152f6e5839c781577d20c163efff
serj99 <go_with_the_flow99@yahoo.com>
2022-05-06 16:21:49 +0300

    Some cleanup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DatamaniputationService } from './services/datamaniputation.service';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { TeslistService } from './teslist.service';
 import { FooterComponent } from './footer/footer.component';
-import { ChangeColorDirective } from './change-color.directive';
-import { SecondLetterCapitalPipe } from './second-letter-capital.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CustomPopupModalComponent } from "./common/custom-popup-modal/custom-popup-modal.component";
-
-import { QuizTypeComponent } from './layout/quiz-type/quiz-type.component';
-import { NotificationComponent } from './layout/notification/notification.component';
-import { TestComponent } from './layout/test/test.component';
-import { ScoreComponent } from './layout/score/score.component';
-import { FeedbackComponent } from './layout/feedback/feedback.component';
-import { ReviewComponent } from './layout/review/review.component';
-import { RegisterComponent } from './layout/register/register.component';
-import { LoginComponent } from './layout/login/login.component';
-import { UserPanelComponent } from './layout/user-panel/user-panel.component';
-
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MatNativeDateModule } from '@angular/material/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './material-module';
-import { DashboardComponent, DashboardDialog } from './layout/dashboard/dashboard.component';
 
+import { CustomPopupModalComponent } from "./common/custom-popup-modal/custom-popup-modal.component";
+import { DashboardComponent, DashboardDialog } from './layout/dashboard/dashboard.component';
+import { FeedbackComponent } from './layout/feedback/feedback.component';
+import { LoginComponent } from './layout/login/login.component';
+import { NotificationComponent } from './layout/notification/notification.component';
+import { QuizTypeComponent } from './layout/quiz-type/quiz-type.component';
+import { RegisterComponent } from './layout/register/register.component';
+import { ReviewComponent } from './layout/review/review.component';
+import { ScoreComponent } from './layout/score/score.component';
+import { TestComponent } from './layout/test/test.component';
+import { UserPanelComponent } from './layout/user-panel/user-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotificationComponent,
-    TestComponent,
-    ScoreComponent,
-    FooterComponent,
-    QuizTypeComponent,
-    ChangeColorDirective,
-    SecondLetterCapitalPipe,
     CustomPopupModalComponent,
+    DashboardComponent,
+    DashboardDialog,
     FeedbackComponent,
+    FooterComponent,
+    LoginComponent,
+    NotificationComponent,
+    QuizTypeComponent,
     ReviewComponent,
     RegisterComponent,
-    LoginComponent,
+    ScoreComponent,
+    TestComponent,
     UserPanelComponent,
-    DashboardComponent,
-    DashboardDialog
   ],
   imports: [
     BrowserModule,
--- src/app/second-letter-capital.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SecondLetterCapitalPipe } from './second-letter-capital.pipe';
-
-describe('SecondLetterCapitalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SecondLetterCapitalPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
